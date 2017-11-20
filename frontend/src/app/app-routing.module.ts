de8dfde0fd12f6b5fa498ea2dcc454196a60a25a
Ankit Bharadiya <asbharadiya@gmail.com>
2017-11-20 01:33:37 -0800

    header bar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 
 import { LandingComponent }   from './components/landing/landing.component';
+import {EducationComponent} from "./components/education/education.component";
+import {AboutUsComponent} from "./components/about-us/about-us.component";
+import {ContactUsComponent} from "./components/contact-us/contact-us.component";
 
 const appRoutes: Routes = [
-  { path: '', component: LandingComponent, pathMatch: 'full' }
+  { path: '', component: LandingComponent, pathMatch: 'full' },
+  { path: 'education', component: EducationComponent },
+  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'contactus', component: ContactUsComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { HeaderComponent } from './components/header/header.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { EducationComponent } from './components/education/education.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LandingComponent
+    LandingComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
