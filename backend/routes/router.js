388730bfbb149c6c0d96aee24ed3debc9019daa9
Ankit Bharadiya <asbharadiya@gmail.com>
2017-11-27 12:57:57 -0800

    banner section

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,14 +2,11 @@
 module.exports = function(router,passport) {
 
 	var auth = require('./auth')(passport);
-    var user = require('./user');
 
 	router.post('/signin', auth.signin);
 	router.post('/signup', auth.signup);
 	router.post('/logout', auth.logout);
 	router.get('/check_session', isAuthenticated, auth.checkSession);
-
-	router.get('/user_profile', user.getUserProfile)
 	
 	function isAuthenticated(req, res, next) {
 		if(req.session.passport && req.session.passport.user._id) {
